72d330593653bc29aaf416929863e1ddb0345b0d
kklein <kleinkevin.lu@gmail.com>
2025-03-17 18:31:06 +0100

    Use variables.

--- payslip.py
+++ payslip.py
@@ -22,24 +22,26 @@ SCOPES = [
     "https://www.googleapis.com/auth/drive.file",
 ]
 
+_TOKEN_FILENAME = "token.json"
 
-def _credentials() -> Credentials:
+
+def _credentials(credentials_filename: str = "credentials.json") -> Credentials:
     creds = None
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists("token.json"):
-        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    if os.path.exists(_TOKEN_FILENAME):
+        creds = Credentials.from_authorized_user_file(_TOKEN_FILENAME, SCOPES)
 
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file(credentials_filename, SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open("token.json", "w") as token:
+        with open(_TOKEN_FILENAME, "w") as token:
             token.write(creds.to_json())
 
     return creds
