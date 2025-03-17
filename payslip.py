bbd1e2559d8b94b071482be0e57bfa8bf61440ba
kklein <kleinkevin.lu@gmail.com>
2025-03-17 18:26:03 +0100

    Annotate more.

--- payslip.py
+++ payslip.py
@@ -5,29 +5,32 @@ from datetime import datetime
 from pathlib import Path
 from tempfile import TemporaryFile
 
+from dataclasses import dataclass
+
 import click
 from git_root import git_root
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
+from googleapiclient.discovery import build, Resource
 from googleapiclient.http import MediaFileUpload
 from pypdf import PdfReader, PdfWriter
 
-# If modifying these SCOPES, delete the file token.pickle.
+# If modifying these SCOPES, delete the file token.json.
 SCOPES = [
     "https://www.googleapis.com/auth/gmail.readonly",
     "https://www.googleapis.com/auth/drive.file",
 ]
 
 
-def _creds():
+def _credentials() -> Credentials:
     creds = None
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
     if os.path.exists("token.json"):
         creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
@@ -42,21 +45,27 @@ def _creds():
     return creds
 
 
-def _gmail_service():
-    return build("gmail", "v1", credentials=_creds())
+def _gmail_service(credentials: Credentials, version: str = "v1") -> Resource:
+    return build("gmail", version, credentials=credentials)
 
 
-def _google_drive_service():
-    return build("drive", "v3", credentials=_creds())
+def _google_drive_service(credentials: Credentials, version: str = "v3") -> Resource:
+    return build("drive", version, credentials=credentials)
 
 
-def search_messages(service, query: str) -> list:
+@dataclass
+class Message:
+    id: str
+    threadId: str
+
+
+def search_messages(service: Resource, query: str) -> list[Message]:
     """Search for messages that match the query."""
     result = service.users().messages().list(userId="me", q=query).execute()
     messages = []
 
     if "messages" in result:
-        messages.extend(result["messages"])
+        messages.extend([Message(**message) for message in result["messages"]])
 
     while "nextPageToken" in result:
         page_token = result["nextPageToken"]
@@ -67,14 +76,16 @@ def search_messages(service, query: str) -> list:
             .execute()
         )
         if "messages" in result:
-            messages.extend(result["messages"])
+            messages.extend([Message(**message) for message in result["messages"]])
 
     return messages
 
 
-def get_attachments(service, message_id: str) -> tuple[list, list]:
+def get_attachments(
+    service: Resource, message_id: str
+) -> tuple[list[bytes], list[str]]:
     """Get and save attachments from a message."""
-    message = (
+    message_content = (
         service.users()
         .messages()
         .get(userId="me", id=message_id, format="full")
@@ -82,19 +93,19 @@ def get_attachments(service, message_id: str) -> tuple[list, list]:
     )
 
     subject = ""
-    for header in message["payload"]["headers"]:
+    for header in message_content["payload"]["headers"]:
         if header["name"] == "Subject":
             subject = header["value"]
-            break
 
-    if "parts" not in message["payload"]:
-        return []
+    if "parts" not in message_content["payload"]:
+        return [], []
 
     saved_attachments = []
     saved_filenames = []
-    parts = message["payload"].get("parts", [])
+    parts = message_content["payload"].get("parts", [])
 
-    def process_parts(parts, prefix=""):
+    # TODO: Annotate.
+    def process_parts(parts, prefix: str = "") -> None:
         nonlocal saved_attachments
         nonlocal saved_filenames
         for part in parts:
@@ -112,6 +123,7 @@ def get_attachments(service, message_id: str) -> tuple[list, list]:
                         .execute()
                     )
 
+                    # TODO: First validate if attachment is a pdf file.
                     file_data = base64.urlsafe_b64decode(attachment["data"])
 
                     saved_attachments.append(file_data)
@@ -124,10 +136,13 @@ def get_attachments(service, message_id: str) -> tuple[list, list]:
                 process_parts(part["parts"], prefix + "--")
 
     process_parts(parts)
+    breakpoint()
     return saved_attachments, saved_filenames
 
 
-def export_wo_password(raw_pdf_data, output_pdf: Path, password: str) -> None:
+def export_pdf_wo_password(
+    raw_pdf_data: bytes, output_pdf: Path, password: str
+) -> None:
     with TemporaryFile() as file_handle:
         file_handle.write(raw_pdf_data)
         reader = PdfReader(file_handle)
@@ -143,10 +158,10 @@ def export_wo_password(raw_pdf_data, output_pdf: Path, password: str) -> None:
     with open(output_pdf, "wb") as output_file:
         writer.write(output_file)
 
-    print(f"Created unprotected PDF: {output_pdf}")
+    print(f"Wrote unencrypted PDF to: {output_pdf}")
 
 
-def upload_file(path: Path, service):
+def upload_file(path: Path, service: Resource):
     media = MediaFileUpload(path)
 
     google_drive_file = (
@@ -160,12 +175,14 @@ def upload_file(path: Path, service):
 
 @click.command()
 @click.password_option("--password")
-@click.option("--subject", default="Lohnabrechnung")
-def main(password, subject):
+@click.option("--subject", default="Lohnabrechnung", type=str)
+def main(password: str, subject: str) -> None:
     export_path = Path(git_root(subject))
     export_path.mkdir(exist_ok=True)
 
-    gmail_service = _gmail_service()
+    credentials = _credentials()
+
+    gmail_service = _gmail_service(credentials)
 
     query = "subject:" + subject
     messages = search_messages(gmail_service, query)
@@ -173,13 +190,15 @@ def main(password, subject):
     print(f"Found {len(messages)} emails matching the query.")
 
     for message in messages:
-        attachments, filenames = get_attachments(gmail_service, message["id"])
+        attachments, filenames = get_attachments(gmail_service, message.id)
         if len(attachments) > 1:
             raise ValueError("Some emails contained more than one attachment.")
 
-        export_wo_password(attachments[0], export_path / filenames[0], password)
+        export_pdf_wo_password(attachments[0], export_path / filenames[0], password)
 
-    google_drive_service = _google_drive_service()
+    google_drive_service = _google_drive_service(credentials)
+
+    # TODO: Check if file already present on Google Drive.
 
     for file_path in export_path.iterdir():
         upload_file(file_path, google_drive_service)
